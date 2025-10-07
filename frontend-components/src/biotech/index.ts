36f18bec0c9e2e789ff73bbc0a4376669e628e99
deathknight2002 <219362658+deathknight2002@users.noreply.github.com>
2025-10-06 20:32:13 -0700

    A
    A

---
+++ frontend-components/src/biotech/index.ts
@@ -0,0 +1,17 @@
+// Biotech-specific components
+// Pharmaceutical and drug development focused components
+
+// Molecules - Biotech data display components  
+export { BioMetricGrid } from './molecules/BioMetricGrid';
+export type { BioMetricGridProps } from './molecules/BioMetricGrid';
+
+export { CatalystTicker } from './molecules/CatalystTicker';
+export type { CatalystTickerProps } from './molecules/CatalystTicker';
+
+export { ClinicalTrialsTimeline } from './molecules/ClinicalTrialsTimeline';
+export type { ClinicalTrialsTimelineProps, ClinicalTrial } from './molecules/ClinicalTrialsTimeline';
+
+// Organisms - Complex biotech dashboards
+export { BioAuroraDashboard } from './organisms/BioAuroraDashboard';
+
+export { BiotechFinancialDashboard } from './organisms/BiotechFinancialDashboard';
